2f1abf4895ea3401a4efdcc41e8c890053ba54c2
Nicolas Torres <nicolas.torres.gussoni@gmail.com>
2021-02-07 19:44:25 +0100

    Adds missing steps

--- index.ts
+++ index.ts
@@ -12,28 +12,28 @@ export default RecipeBuilder()
   )
   .setOwner("ntorres.dev@gmail.com")
   .setRepoLink("https://github.com/ntgussoni/blitz-guard")
-  // .addAddDependenciesStep({
-  //   stepId: "addDeps",
-  //   stepName: "Add npm dependencies",
-  //   explanation: `Adding Blitz Guard dependencies`,
-  //   packages: [{ name: "@blitz-guard/core", version: "latest" }],
-  // })
-  // .addNewFilesStep({
-  //   stepId: "addApiRoute",
-  //   stepName: "Add Api Route ",
-  //   explanation: `We'll setup a query API endpoint to for you to check your rules with useQuery`,
-  //   targetDirectory: "app/guard/queries/getAbility.ts",
-  //   templatePath: join(__dirname, "templates", "queries", "getAbility.ts"),
-  //   templateValues: {},
-  // })
-  // .addNewFilesStep({
-  //   stepId: "addAbilityFile",
-  //   stepName: "Add Ability File ",
-  //   explanation: `One file to rule them all, this is the main file where your rules will live. Creating that for you now.`,
-  //   targetDirectory: "app/guard/ability.ts",
-  //   templatePath: join(__dirname, "templates", "ability.ts"),
-  //   templateValues: {},
-  // })
+  .addAddDependenciesStep({
+    stepId: "addDeps",
+    stepName: "Add npm dependencies",
+    explanation: `Adding Blitz Guard dependencies`,
+    packages: [{ name: "@blitz-guard/core", version: "latest" }],
+  })
+  .addNewFilesStep({
+    stepId: "addApiRoute",
+    stepName: "Add Api Route ",
+    explanation: `We'll setup a query API endpoint to for you to check your rules with useQuery`,
+    targetDirectory: "app/guard/queries/getAbility.ts",
+    templatePath: join(__dirname, "templates", "queries", "getAbility.ts"),
+    templateValues: {},
+  })
+  .addNewFilesStep({
+    stepId: "addAbilityFile",
+    stepName: "Add Ability File ",
+    explanation: `One file to rule them all, this is the main file where your rules will live. Creating that for you now.`,
+    targetDirectory: "app/guard/ability.ts",
+    templatePath: join(__dirname, "templates", "ability.ts"),
+    templateValues: {},
+  })
   .addTransformFilesStep({
     stepId: "addMiddleware",
     stepName: "Add development middleware",
